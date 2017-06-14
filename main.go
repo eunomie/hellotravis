46cdb21c1b88c24ffb762327665e4e53f9a9b7dd
Yves Brissaud <eunomie@squarescale.com>
2017-06-14 16:51:27 +0200

    Add env vars and hostname

--- main.go
+++ main.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!!!"))
+
+	fmt.Fprintf(w, "\nEnvironment:\n")
+	for _, env := range os.Environ() {
+		fmt.Fprintf(w, "%s\n", env)
+	}
+	fmt.Fprintf(w, "\n")
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s\n", err)
+		return
+	}
+	fmt.Fprintf(w, "Generated on %s\n", hostname)
 }
 
 func main() {
